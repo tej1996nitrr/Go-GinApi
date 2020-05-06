b0dea7ec18e9bec24f9e3300d58d7f2047507f98
tejas1996nitrr <tejaskanikdaley1996@gmail.com>
2020-05-06 12:52:33 +0530

    implemented video services

--- main.go
+++ main.go
@@ -1,69 +1,17 @@
 package main
 
 import (
-	"net/http"
-	"strconv"
-
-	"github.com/gin-gonic/contrib/static"
 	"github.com/gin-gonic/gin"
 )
 
 // Joke contains information about a single Joke
-type Joke struct {
-	ID    int    `json:"id" binding:"required"`
-	Likes int    `json:"likes"`
-	Joke  string `json:"joke" binding:"required"`
-}
-
-//We'll create a list of jokes
-var jokes = []Joke{
-	Joke{1, 0, "Did you hear about the restaurant on the moon? Great food, no atmosphere."},
-	Joke{2, 0, "What do you call a fake noodle? An Impasta."},
-	Joke{3, 0, "How many apples grow on a tree? All of them."},
-	Joke{4, 0, "Want to hear a joke about paper? Nevermind it's tearable."},
-	Joke{5, 0, "I just watched a program about beavers. It was the best dam program I've ever seen."},
-	Joke{6, 0, "Why did the coffee file a police report? It got mugged."},
-	Joke{7, 0, "How does a penguin build it's house? Igloos it together."},
-}
-
 func main() {
-	router := gin.Default()
-	router.Use(static.Serve("/", static.LocalFile("./views", true)))
-	api := router.Group("/api")
-	{
-		api.GET("/", func(c *gin.Context) {
-			c.JSON(http.StatusOK, gin.H{
-				"message": "pong",
-			})
+	server := gin.Default()
+	server.GET("/ping", func(c *gin.Context) {
+		c.JSON(200, gin.H{
+			"message": "pong",
 		})
-	}
-	api.GET("/jokes", JokeHandler)
-	api.POST("/jokes/like/:jokeID", LikeJoke)
-	router.Run(":3000")
-}
+	})
+	server.Run(":8080")
 
-//JokeHandler retrieves all jokes items list
-func JokeHandler(c *gin.Context) {
-	c.Header("Content-Type", "application/json")
-	c.JSON(http.StatusOK, jokes)
-}
-
-// LikeJoke increments the like of a particular joke
-func LikeJoke(c *gin.Context) {
-	// confirm Joke ID sent is valid
-
-	if jokeid, err := strconv.Atoi(c.Param("jokeID")); err == nil {
-		// find joke, and increment likes
-		for i := 0; i < len(jokes); i++ {
-			if jokes[i].ID == jokeid {
-				jokes[i].Likes += 1
-			}
-		}
-
-		// return a pointer to the updated jokes list
-		c.JSON(http.StatusOK, &jokes)
-	} else {
-		// Joke ID is invalid
-		c.AbortWithStatus(http.StatusNotFound)
-	}
 }
